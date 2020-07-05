c6ea380ed59d5fd0236087e42ec5f0ad3212c55b
shunsuke-ba <monkey_shun.baske@icloud.com>
2020-07-05 23:17:02 +0900

    add until main.go L160 read CALENDAR_ID

--- main.go
+++ main.go
@@ -1,16 +1,135 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"os/user"
+	"path/filepath"
 
 	"github.com/line/line-bot-sdk-go/linebot"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
 )
 
 type PostRequest struct {
 	PostContent string `json:"post"`
+}
+
+func DefaultMessage(bot *linebot.Client, event *linebot.Event) error {
+	reply := linebot.NewTextMessage("今日も志を忘れず頑張ってください！！")
+	if _, err := bot.ReplyMessage(event.ReplyToken, reply).Do(); err != nil {
+		log.Print(err)
+		return err
+	}
+	return nil
+}
+
+func tokenCacheFile() (string, error) {
+	user, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	tokenCacheDir := filepath.Join(user.HomeDir, ".credentials")
+	os.MkdirAll(tokenCacheDir, 0700)
+	return filepath.Join(tokenCacheDir, url.QueryEscape("// ちょっとわからない")), err
+	// ちょっとわからない = "generate-schedule-calendar.json"
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
+	return t, err
+}
+
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the authorization code: \n%v\n", authURL)
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tok
+}
+
+func saveToken(file string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.Create(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+	tok, err := tokenFromFile(cacheFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(cacheFile, tok)
+	}
+	return config.Client(ctx, tok)
+}
+
+func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	cacheFile, err := tokenCacheFile()
+	errorLog("Unable to get path to cached credential file.: ", err)
+
+	tok, err := tokenFromFile(cacheFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(cacheFile, tok)
+	}
+	return config.Client(ctx, tok)
+}
+
+func NewGoogleAPI() (*calendar.Service, error) {
+	ctx := context.Background()
+	json, err := ioutil.ReadFile("client-secret.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	config, err := google.ConfigFromJSON(json, calendar.CalendarScope)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+	tok, err := tokenFromFile(cacheFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(cacheFile, tok)
+	}
+	client := config.Client(ctx, tok)
+	NewCalen, err := calendar.New(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return NewCalen, nil
 }
 
 func CallCalen(bot *linebot.Client, event *linebot.Event) error {
@@ -31,18 +150,14 @@ func CallCalen(bot *linebot.Client, event *linebot.Event) error {
 	return nil
 }
 
-func DefaultMessage(bot *linebot.Client, event *linebot.Event) error {
-	reply := linebot.NewTextMessage("今日も志を忘れず頑張ってください！！")
-	if _, err := bot.ReplyMessage(event.ReplyToken, reply).Do(); err != nil {
-		log.Print(err)
-		return err
-	}
-	return nil
-}
-
 func PostBack(bot *linebot.Client, event *linebot.Event) error {
 	datetime := event.Postback.Params.Datetime
 	log.Printf("here is postback %v\n", datetime)
+	calen, err := NewGoogleAPI()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//_, err = calen.Events.Insert(CALENDAR_ID, createEventData(schedule)).Do()
 
 	// Throw a request here
 	// POST https://www.googleapis.com/calendar/v3/calendars/calendarId/acl
